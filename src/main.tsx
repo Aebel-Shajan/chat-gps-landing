a3f23f8899b8db18cc0e1fc6a82d88aa7ff2f86e
aebel-shajan <aebelshajan@gmail.com>
2025-11-03 09:12:09 +0000

    use hash router instead.

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate, } from 'react-router-dom'
+import { Routes, Route, Navigate, HashRouter, } from 'react-router-dom'
 import './index.css'
 import PrivacyPolicy from './pages/PrivacyPolicy.tsx'
 import Home from './pages/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/chat-gps-landing">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="privacy-policy" element={<PrivacyPolicy />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
